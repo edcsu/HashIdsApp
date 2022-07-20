e493e312cdd5d88c4857bd6d854413bafb90ac4b
Sewannonda Keith Edwin <skeith696@gmail.com>
2022-07-20 13:09:36 +0300

    feat: add hashids

--- HashIdsApp/Controllers/WeatherForecastController.cs
+++ HashIdsApp/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using HashidsNet;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HashIdsApp.Controllers
@@ -21,8 +22,16 @@ namespace HashIdsApp.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            var hashids = new Hashids("this is my salt");
+
+            //var newId = Guid.NewGuid();
+            var newId = Random.Shared.Next(5, 55);
+            var hash = hashids.Encode(newId);
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
+                Id = newId,
+                ShortId = hash,
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
--- HashIdsApp/WeatherForecast.cs
+++ HashIdsApp/WeatherForecast.cs
@@ -2,7 +2,7 @@ namespace HashIdsApp
 {
     public class WeatherForecast
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
 
         public string? ShortId { get; set; }
         
